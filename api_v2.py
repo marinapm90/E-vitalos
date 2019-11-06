eb62d62056b139821bd62d0e9946719979e4c3f8
marinapm90 <mpm.marinaperez@gmail.com>
2019-11-06 02:03:45 +0100

    new try

--- api_v2.py
+++ api_v2.py
@@ -3,11 +3,30 @@ import json
 from flask import Flask, jsonify
 import requests
 from flask import request
-
+from flask_cors import CORS
+import re
 
     
 app = Flask(__name__)
+CORS(app)
 
+
+@app.route("/additives/bulk", methods=["POST"])
+def find_text():
+    text = request.get_json()
+    result = re.findall(r'E-\d+', str(text))
+    result_2 = re.findall(r'E\d+', str(text))
+    
+    values = []
+    for value in result:        
+        values.append(value)
+    for value in result_2:       
+        values.append(value)
+    for value in values:
+        if "E-" in value:
+            values = [item.replace('E-','E') for item in values]           
+        else:
+            return jsonify(values)
 
 
 @app.route('/', methods=['GET'])
@@ -33,11 +52,11 @@ def additive(additive):
 def bulk_resolve_additives():
     with open('./additives.json', 'r') as jsonfile:
         file_data = json.load(jsonfile)
-    additives_dict = request.get_json()    
-    additives = additives_dict["additives"]
+    
     result = {}
-    for additive in additives:
-        result[additive] = file_data[additive]
+    
+    for value in values:
+        result[value] = file_data[value]
     
     return jsonify(result)
 
